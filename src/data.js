19e35598f5948c8f77bafff58ea321863c97c729
Franco <fnosettoecheverria@hotmail.com>
2022-12-07 21:37:50 -0300

    Creando carrito

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        id: 1,
+        nameProduct: 'Pokemon Heart Gold',
+        price: 70,
+        urlImage: 'https://img.pokemondb.net/boxes/lg/heartgold-large.jpg',
+        quantity: 1,
+    },
+    {
+        id: 2,
+        nameProduct: 'Pokemon Soul Silver',
+        price: 70,
+        urlImage: 'https://img.pokemondb.net/boxes/lg/soulsilver-large.jpg',
+        quantity: 1,
+    },
+    {
+        id: 3,
+        nameProduct: 'Pokemon Black',
+        price: 90,
+        urlImage: 'https://img.pokemondb.net/boxes/lg/black-large.jpg',
+        quantity: 1,
+    },
+    {
+        id: 4,
+        nameProduct: 'Pokemon White',
+        price: 90,
+        urlImage: 'https://img.pokemondb.net/boxes/lg/white-large.jpg',
+        quantity: 1,
+    },
+    {
+        id: 5,
+        nameProduct: 'Pokemon X',
+        price: 100,
+        urlImage: 'https://img.pokemondb.net/boxes/lg/x-large.jpg',
+        quantity: 1,
+    },
+    {
+        id: 6,
+        nameProduct: 'Pokemon Y',
+        price: 100,
+        urlImage: 'https://img.pokemondb.net/boxes/lg/y-large.jpg',
+        quantity: 1,
+    },
+]
